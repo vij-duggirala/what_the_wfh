0104f9c5910065d51043d4f549b822cdcb7e39cc
vij.duggirala <viji9810@gmail.com>
2021-02-13 20:23:03 +0530

    basic outline

---
+++ index.js
@@ -0,0 +1,137 @@
+const electron = require('electron')
+const url = require('url')
+const path = require('path')
+
+const schedule = require('node-schedule');
+
+const { PythonShell } = require('python-shell');
+let pyshell = new PythonShell('sample.py')
+
+const { app, BrowserWindow, Menu, ipcMain } = electron;
+
+let mainWindow;
+let timerWindow;
+
+let prev_message = "b'What The WFH'";
+
+app.on('ready', function () {
+
+    mainWindow = new BrowserWindow({
+        webPreferences: {
+            contextIsolation: false,
+            nodeIntegration: true
+        }
+    });
+
+    mainWindow.loadURL(url.format({
+        pathname: path.join(__dirname, 'landing.html'),
+        protocol: 'file',
+        slashes: true
+    }));
+
+    const mainMenu = Menu.buildFromTemplate(menu);
+    Menu.setApplicationMenu(mainMenu);
+
+    mainWindow.on('closed', function () {
+        app.quit();
+    })
+
+
+    startTimer();
+
+
+});
+
+
+function startTimer() {
+    let display = electron.screen.getPrimaryDisplay();
+    let width = display.bounds.width;
+    timerWindow = new BrowserWindow({
+        width: 200,
+        height: 100,
+        title: 'Timer',
+        webPreferences: {
+            contextIsolation: false,
+            nodeIntegration: true
+        },
+        x: width - 200,
+        y: 0
+    });
+
+    timerWindow.loadURL(url.format({
+        pathname: path.join(__dirname, 'timer.html'),
+        protocol: 'file',
+        slashes: true
+    }));
+
+
+    timerWindow.setAlwaysOnTop(true, 'screen');
+
+    timerWindow.on('close', function () {
+        timerWindow = null;
+    })
+
+}
+
+
+
+const menu = [
+    {
+        label: 'File',
+        submenu: [
+            {
+                label: 'Quit',
+                click() {
+                    startTimer();
+                }
+            },
+            {
+                label: 'DevTools',
+                click(item, focusedWindow) {
+                    focusedWindow.toggleDevTools();
+                }
+            }
+        ]
+    }
+]
+
+var os_type;
+if (process.platform == 'darwin') {
+    menu.unshift({});
+    os_type = 'mac';
+}
+else if (process.platform == 'linux')
+    os_type = 'linux';
+else
+    os_type = 'windows';
+
+
+console.log(os_type);
+
+
+ipcMain.on('time', function (e, time) {
+    //mongo 
+    console.log(time);
+});
+
+
+
+
+var cls = 1;
+pyshell.on('message', function (message) {
+
+    if (message === "b'Ticking...'") {
+        //kjhk
+    }
+    else {
+        console.log(message);
+        if (message != prev_message) {
+            console.log('there');
+            prev_message = message;
+            console.log(timerWindow);
+            timerWindow.webContents.send('close', cls);
+            startTimer();
+        }
+    }
+
+})
---
+++ sample.py
@@ -0,0 +1,39 @@
+import sys
+import os
+import subprocess
+import re
+import schedule
+import time
+
+
+def get_active_window_title():
+    root = subprocess.Popen(
+        ['xprop', '-root', '_NET_ACTIVE_WINDOW'], stdout=subprocess.PIPE)
+    stdout, stderr = root.communicate()
+
+    m = re.search(b'^_NET_ACTIVE_WINDOW.* ([\w]+)$', stdout)
+    if m != None:
+        window_id = m.group(1)
+        window = subprocess.Popen(
+            ['xprop', '-id', window_id, 'WM_NAME'], stdout=subprocess.PIPE)
+        stdout, stderr = window.communicate()
+    else:
+        return None
+
+    match = re.match(b"WM_NAME\(\w+\) = (?P<name>.+)$", stdout)
+    if match != None:
+        return match.group("name").strip(b'"')
+
+    return None
+
+
+def printt():
+    if __name__ == "__main__":
+        print(get_active_window_title())
+        sys.stdout.flush()
+
+
+schedule.every(0.0083).minutes.do(printt)
+while 1:
+    schedule.run_pending()
+    time.sleep(1)
